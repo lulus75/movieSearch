a3003315fd52d1b23526ae4132d78559e0f079d8
lulus75 <antoine_mailly@hotmail.com>
2016-03-31 18:21:31 +0200

    Update Connexion.php

--- src/MovieSearch/Connexion.php
+++ src/MovieSearch/Connexion.php
@@ -6,7 +6,7 @@ class Connexion{
     private function __construct(){
         $config = new \Doctrine\DBAL\Configuration();
         $connectionParams = array(
-            'url' => 'mysql://root:@127.0.0.1/movie?charset=utf8mb4',
+            'url' => 'mysql://User:pwd@127.0.0.1/BDD_name?charset=utf8mb4',
         );
         $this->conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
     }
